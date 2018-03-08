91515fe1073801f1ab54e67b32a6787d6870d33b
Bram Yuh <bram@actacom.nl>
2018-03-08 09:26:34 +0100

    puppet manifest for copying bcld

---
+++ copybcld.pp
@@ -0,0 +1,26 @@
+package { 'apache2':
+  ensure => installed,
+}
+
+service { 'apache2':
+  ensure => running
+  enable => true
+  require => Package['apache2'],
+
+file { "/var/www/html/bcld/":
+  ensure => "directory",
+  require => Package['apache2'],
+}
+
+file { "/var/www/html/bcld/vmlinuz":
+  ensure => present,
+  source => /home/administrator/git/facet/generic/vmlinuz,
+  require => File['/var/www/html/bcld'],
+}
+
+file { "/var/www/html/bcld/initrd":
+  ensure => present
+  source => /home/administrator/git/facet/generic/vmlinuz,
+  require => File['/var/www/html/bcld'],
+}
+
